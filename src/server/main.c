a05ecded565d8e992cc5d433cc909f03d6665259
Aditya <barawkar@usc.edu>
2014-11-14 20:52:16 -0800

    change to test RX SO_TIMESTAMP functionality

--- src/server/main.c
+++ src/server/main.c
@@ -2,12 +2,13 @@
 #include "sniffer.h"
 int main(int argc, char *argv[])
 {
+    /*
     struct sniffer_arg arg; 
 
     arg.dev = SENDER_INF;
-    //arg.my_addr = DRTT_RECEIVER_ADDR;
     arg.my_addr = STRATUM1;
-    //arg.my_addr = STRATUM0;
     start_sniffer((void*)&arg);
+    */
+    start_receiver(NULL);
     return 0;
 }
