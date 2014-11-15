3b3e0f48cda428376e32df321b313b7c32035c75
Aditya <barawkar@usc.edu>
2014-11-14 21:16:15 -0800

    declared structures in recv_ts.c and passed their addresses

--- src/server/recv_ts.c
+++ src/server/recv_ts.c
@@ -2,12 +2,12 @@
 void*
 start_receiver(void *argument)
 {
-    struct msghdr* msg;
-    struct iovec* entry;
-    struct control* control;
+    struct msghdr msg;
+    struct iovec entry;
+    struct control control;
     char *payload;
     int payload_len = 1024; //[AB] put in config
-    struct sockaddr_in* from_addr;
+    struct sockaddr_in from_addr;
     int ret;
     int s;
 
@@ -15,16 +15,16 @@ start_receiver(void *argument)
     memset(payload, 0, payload_len);
    
     s = create_recv_socket_ts("eth0", 5555);
-    setup_msghdr(msg, 
-                 entry, 
-                 control,  
+    setup_msghdr(&msg, 
+                 &entry, 
+                 &control,  
                  payload, 
                  payload_len, 
-                 from_addr);
+                 &from_addr);
 
     while(1)
     {
-        ret = recv_packet_ts(s, msg, 0);
+        ret = recv_packet_ts(s, &msg, 0);
         if (ret < 0){
             printf("Error receiving\n");
             exit(1);
