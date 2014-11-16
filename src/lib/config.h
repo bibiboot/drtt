2b0760dd5973e44771ae089693ec981cb22fd33f
dan <rehman.d.fazlur@gmail.com>
2014-11-16 12:19:20 -0800

    Adding new changes for ipc

--- src/lib/config.h
+++ src/lib/config.h
@@ -2,12 +2,14 @@
 
 #define CONFIG_INIT { \
     .packet_len = 65535 , \
-    .rtt = 2 ,} 
+    .ipc_sleep_time = 60*1, \
+    .rtt = 2 ,}
 
 struct config {
     uint16_t rtt;
     uint16_t packet_len;
+    uint16_t ipc_sleep_time;
 };
 
 #define SENDER_INF "eth0"
-#define DRTT_RECEIVER_ADDR 3 
+#define DRTT_RECEIVER_ADDR 3
--- src/lib/globals.h
+++ src/lib/globals.h
@@ -17,7 +17,18 @@
 #include "cprotocol.h"
 #include "uthash.h"
 
-FILE *LOGFILE;
+#define CUSTOM_HEADER_SZ sizeof(struct custom_packet_header)
+#define TIMESTAMP_SZ sizeof(struct timestamp)
+#define DRTT_SZ (CUSTOM_HEADER_SZ + TIMESTAMP_SZ)
+#define STRATUM0 1
+#define STRATUM1 2
+#define DRTT_REQ_PORT 1
+#define DRTT_RES_PORT 2
+#define BROADCAST_ADDR 0xffff
+
+//FILE *LOGFILE;
+typedef uint16_t addr_t;
+typedef uint16_t port_t;
 
 typedef struct hashl {
     int key;
@@ -25,30 +36,24 @@ typedef struct hashl {
     UT_hash_handle hh;
 } dict_node;
 
-struct globals {
-    struct config config;
-    dict_node *hashl;
+struct custom_packet_header{
+    addr_t next_hop_addr;
+    addr_t dst_addr;
+    addr_t src_addr;
+    port_t dst_port;
 };
 
-typedef uint16_t addr_t;
-typedef uint16_t port_t;
-struct custom_packet_header{                                                       
-    addr_t next_hop_addr;                                                     
-    addr_t dst_addr;                                                          
-    addr_t src_addr;                                                          
-    port_t dst_port;                                                          
-};
 struct timestamp{
     uint32_t sec;
     uint32_t fsec;
 };
-extern struct globals globals;
 
-#define CUSTOM_HEADER_SZ sizeof(struct custom_packet_header)                       
-#define TIMESTAMP_SZ sizeof(struct timestamp)                                           
-#define DRTT_SZ (CUSTOM_HEADER_SZ + TIMESTAMP_SZ)
-#define STRATUM0 1
-#define STRATUM1 2
-#define DRTT_REQ_PORT 1
-#define DRTT_RES_PORT 2
-#define BROADCAST_ADDR 0xffff
+struct globals {
+    struct config config;
+    dict_node *hashl;
+    pthread_t recv_th;
+    pthread_t ipc_th;
+    char *drtt;
+};
+
+extern struct globals globals;
