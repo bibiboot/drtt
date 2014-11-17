eb6a79f868f7899a1c4e007f4ce4ea2818356761
Aditya <barawkar@usc.edu>
2014-11-17 12:33:35 -0800

    memset was not done on proper address

--- src/server/recv_lib.c
+++ src/server/recv_lib.c
@@ -1,21 +1,25 @@
 #include "recv_lib.h"
 void 
-setup_receiver(struct receiver_arg* arg, int* s, char* payload, 
+setup_receiver(struct receiver_arg* arg, int* s, char** payload, 
 			   int payload_len, struct msghdr* msg, struct iovec* entry, 
 			   struct control* control, struct sockaddr_in* from_addr)
 {                                         
     arg->inf_index = inf_to_index(arg->dev);                                    
     arg->send_sfd = create_sending_socket(arg->dev, &(arg->sk));                
                                                                                 
-    payload = (char *)malloc(payload_len);                                      
-    memset(payload, 0, payload_len);                                            
+    *payload = (char *)malloc(payload_len);        
+	if (NULL == payload){
+		printf("malloc failed\n");
+		exit(1);
+	}                              
+    memset(*payload, 0, payload_len);                                            
                                                                                 
     *s = create_recv_rawsocket_ts(arg->dev);                                     
     set_promisc(arg->dev, *s);                                                   
     setup_raw_msghdr(msg,                                                      
                  entry,                                                        
                  control,                                                      
-                 payload,                                                       
+                 *payload,                                                       
                  payload_len,                                                   
                  from_addr);      
 }
