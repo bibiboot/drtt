b098ce187409428d543cdf06ec92938ba9362d82
Student-sc558af USC558L <sc558af@users.isi.deterlab.net>
2014-11-17 17:19:52 -0800

    header file general checkin

--- src/server/recv_lib.h
+++ src/server/recv_lib.h
@@ -4,6 +4,7 @@
 #include "packet_filter.h"                                                      
 #include "create_drtt_response.h"                                               
 #include "my_socket.h" 
+#include "create_timestamp.h"
 struct receiver_arg                                                             
 {                                                                               
     char* dev;             /* device name */                                    
@@ -15,3 +16,6 @@ struct receiver_arg
   
 void setup_receiver(struct receiver_arg*, int*, char**, int, struct msghdr*,
 					struct iovec*, struct control*, struct sockaddr_in*);
+int get_cntp_hops(char *, int);
+int get_cpacket_payload_len(char *, int); 
+uint64_t cal_offset(char *pkt, int total_sz, struct timestamp* s1_rx_ts)
